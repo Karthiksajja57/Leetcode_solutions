98133e7e286e2223ef729bf81a50791da1364947
Karthiksajja57 <dineshkarthiksajja@gmail.com>
2026-09-15 14:32:56 +0530

    Time: 26 ms (49.58%) | Memory: 149.2 MB (59.37%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,38 +1,27 @@
 class NumMatrix {
 public:
-
-    vector<vector<int>> arr;
+    vector<vector<int>> p;
 
     NumMatrix(vector<vector<int>>& matrix) {
-        // row - Sum 
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-        for(int i = 0; i < n; i++){
-            for(int j = 1; j < m; j++){
-                matrix[i][j] = matrix[i][j-1] + matrix[i][j];
+        p.assign(m + 1, vector<int>(n + 1));
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                p[i][j] = matrix[i - 1][j - 1]
+                        + p[i - 1][j]
+                        + p[i][j - 1]
+                        - p[i - 1][j - 1];
             }
         }
-        for(int j = 0; j < m; j++){
-            for(int i = 1; i < n; i++){
-                matrix[i][j] = matrix[i-1][j] + matrix[i][j];
-            }
-        }
-
-        arr = matrix;
-
     }
-    
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        int leftRegion = ((col1 - 1) >= 0) ? arr[row2][col1-1] : 0;
-        int rightRegion = ((row1 - 1) >= 0) ? arr[row1-1][col2] : 0;
-        int doublyReduced = ((col1 - 1) >= 0 && (row1 - 1) >= 0) ? arr[row1-1][col1-1] : 0;
-        return arr[row2][col2] - leftRegion - rightRegion + doublyReduced;
+
+    int sumRegion(int r1, int c1, int r2, int c2) {
+        return p[r2 + 1][c2 + 1]
+             - p[r1][c2 + 1]
+             - p[r2 + 1][c1]
+             + p[r1][c1];
     }
 };
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
