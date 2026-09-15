c12866961c4aa827d832195e236a0c2a7b97d75c
Karthiksajja57 <dineshkarthiksajja@gmail.com>
2026-09-15 14:21:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (50.42%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isdigit(int a){
+        return a >= '0' && a <= '9'; 
+    }
+    string clearDigits(string s) {
+        int i = 0;
+        int n = s.size();
+        while(i < n){
+            if(isdigit(s[i])){
+                s.erase(i, 1);
+                if(i > 0){
+                    s.erase(i - 1, 1);
+                    i--;
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        return s;
+    }
+};
